ea10931e3683763b18ef1e8ee6a3ebca6cea13bd
Loudbook <loudbooked@gmail.com>
2022-10-23 23:28:16 -0400

    ModelEngine API

--- src/main/java/me/loudbook/Main.java
+++ src/main/java/me/loudbook/Main.java
@@ -1,5 +1,7 @@
 package me.loudbook;
 
+import com.ticxo.modelengine.api.ModelEngineAPI;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
